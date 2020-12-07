22fdac507e0bbf3f37c26b3b466990d499aca18b
Ammar <ammarraneez@gmail.com>
2020-12-07 22:45:00 +0530

    get method with parameter

--- springboot-backend/restful-web-services/src/main/java/com/todo/restfulwebservices/HelloWorldController.java
+++ springboot-backend/restful-web-services/src/main/java/com/todo/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.todo.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //can handle rest requests
@@ -26,5 +27,11 @@ public class HelloWorldController {
     @GetMapping(path="/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello world");
+    }
+
+    //path param
+    @GetMapping(path="/hello-world/{name}")
+    public HelloWorldBean helloWorldPath(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello %s", name));
     }
 }
