fd31d47dbd4d78c35417990651dce0016b45a3c2
Ammar <ammarraneez@gmail.com>
2020-12-08 00:27:06 +0530

    cors policy

--- angular-todolist-springboot/src/app/components/list-todos/list-todos.component.ts
+++ angular-todolist-springboot/src/app/components/list-todos/list-todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoDataService } from 'src/app/services/data/todoData/todo-data.service';
 
 @Component({
   selector: 'app-list-todos',
@@ -7,30 +8,37 @@ import { Todo } from 'src/app/models/Todo';
   styleUrls: ['./list-todos.component.css']
 })
 export class ListTodosComponent implements OnInit {
-  todos : Todo[] = [
-    {
-      id: 1,
-      description: "A todo",
-      done: false,
-      targetDate: new Date()
-    },
-    {
-      id: 2,
-      description: "Another todo",
-      done: false,
-      targetDate: new Date()
-    },
-    {
-      id: 3,
-      description: "third todo",
-      done: false,
-      targetDate: new Date()
-    }
-  ]
+  todos: Todo[] = [];
+  // todos : Todo[] = [
+  //   {
+  //     id: 1,
+  //     description: "A todo",
+  //     done: false,
+  //     targetDate: new Date()
+  //   },
+  //   {
+  //     id: 2,
+  //     description: "Another todo",
+  //     done: false,
+  //     targetDate: new Date()
+  //   },
+  //   {
+  //     id: 3,
+  //     description: "third todo",
+  //     done: false,
+  //     targetDate: new Date()
+  //   }
+  // ]
 
-  constructor() { }
+  constructor(private todoService : TodoDataService) { }
 
   ngOnInit(): void {
+    this.todoService.retrieveAllTodos("Ammar").subscribe(
+      response => {
+        this.todos = response;
+        console.log(response)
+      }
+    )
   }
 
 }
--- springboot-backend/restful-web-services/src/main/java/com/todo/restfulwebservices/TodoResource.java
+++ springboot-backend/restful-web-services/src/main/java/com/todo/restfulwebservices/TodoResource.java
@@ -1,11 +1,13 @@
 package com.todo.restfulwebservices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 
+@CrossOrigin()
 @RestController
 public class TodoResource {
     @Autowired
