6cc32f41018d22b1b32993dc4769b386ade398f8
Ammar <ammarraneez@gmail.com>
2020-12-08 00:27:33 +0530

    list of todos sent from backend to frontend

---
+++ angular-todolist-springboot/src/app/services/data/todoData/todo-data.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoDataService {
+  constructor(private httpClient : HttpClient) { }
+
+  retrieveAllTodos(username) {
+    return this.httpClient.get<Todo[]>(`http://localhost:8081/users/${username}/todos`)
+  }
+}
