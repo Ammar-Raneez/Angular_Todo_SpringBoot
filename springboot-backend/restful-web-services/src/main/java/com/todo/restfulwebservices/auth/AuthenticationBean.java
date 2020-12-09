a50a2e136904b8d60702a95dabe421a0504c5aa3
Ammar <ammarraneez@gmail.com>
2020-12-09 21:49:30 +0530

    basic authentication started on frontend

---
+++ springboot-backend/restful-web-services/src/main/java/com/todo/restfulwebservices/auth/AuthenticationBean.java
@@ -0,0 +1,6 @@
+package com.todo.restfulwebservices.auth;
+
+public class AuthenticationBean {
+    public AuthenticationBean(String you_are_authenticated) {
+    }
+}
---
+++ springboot-backend/restful-web-services/src/main/java/com/todo/restfulwebservices/auth/BasicAuthenticationController.java
@@ -0,0 +1,39 @@
+package com.todo.restfulwebservices.auth;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+//can handle rest requests
+@CrossOrigin()
+@RestController
+public class HelloWorldController {
+    //GET
+    //URI - /hello-world
+    //method - "hello world"
+    //GET request mapping
+
+//    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+//    public String helloWorld() {
+//        return "Hello World";
+//    }
+
+    //when you go to /hello-world and perform a get request
+    //hello world is returned to the browser
+//    @GetMapping(path = "/hello-world")
+//    public String helloWorld() {
+//        return "Hello World";
+//    }
+//
+//    @GetMapping(path="/hello-world-bean")
+//    public HelloWorldBean helloWorldBean() {
+//        return new HelloWorldBean("Hello world");
+//    }
+
+    //path param
+    @GetMapping(path="/basicauth")
+    public AuthenticationBean helloWorldPath() {
+        return new AuthenticationBean("You are authenticated");
+    }
+}
