c6db0e8e4966a25fd087485601f584142019f456
Ammar <ammarraneez@gmail.com>
2020-12-10 16:47:49 +0530

    db issue

--- angular-todolist-springboot/src/app/services/data/todoData/todo-data.service.ts
+++ angular-todolist-springboot/src/app/services/data/todoData/todo-data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { API_URL } from 'src/app/app.constants';
+import { API_URL, TODO_JPA_API_URL } from 'src/app/app.constants';
 import { Todo } from 'src/app/models/Todo';
 
 @Injectable({
@@ -10,10 +10,10 @@ export class TodoDataService {
   constructor(private httpClient : HttpClient) { }
 
   retrieveAllTodos(username) {
-    return this.httpClient.get<Todo[]>(`${API_URL}/users/${username}/todos`)
+    return this.httpClient.get<Todo[]>(`${TODO_JPA_API_URL}/users/${username}/todos`)
   }
   retrieveTodo(username, id) {
-    return this.httpClient.get<Todo>(`${API_URL}/users/${username}/todos/${id}`)
+    return this.httpClient.get<Todo>(`${TODO_JPA_API_URL}/users/${username}/todos/${id}`)
   }
 
   deleteTodo(username, id) {
